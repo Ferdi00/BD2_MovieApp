81d66bc23a0ac8c151529b59329e12b3fb067d4f
Ferdinando Boccia <fernandoboccia7@gmail.com>
2024-06-06 18:58:33 +0200

    Modifica homepage e aggiunta filtri

--- app/run.py
+++ app/run.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, render_template, request
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import re
+import re, sys
 
 
 app = Flask(__name__)
+
+# Imposta l'encoding a UTF-8 per la console
+sys.stdout.reconfigure(encoding="utf-8")
 
 # Configurazione del client MongoDB
 client = MongoClient("mongodb://localhost:27017/")
@@ -41,8 +44,6 @@ def search_movies():
     for movie in movies:
         movie["_id"] = str(movie["_id"])
 
-    print(movies)
-
     return jsonify(movies)
 
 
@@ -53,20 +54,39 @@ def movie_detail(movie_id):
     return render_template("movie_detail.html", movie=movie)
 
 
-# Endpoint per ottenere film per genere
-@app.route("/movies/genre/<genre>", methods=["GET"])
-def get_movies_by_genre(genre):
-    movies = list(db.movies.find({"genre": genre}, {"_id": 0}))
-    if movies:
-        return jsonify(movies)
-    else:
-        return jsonify({"error": "No movies found for this genre"}), 404
+@app.route("/api/filter", methods=["GET"])
+def filter_movies():
+    genres = request.args.getlist("genre")
+    rating = request.args.get("rating", "")
+    min_duration = request.args.get("min_duration", "")
+    max_duration = request.args.get("max_duration", "")
+    filter_criteria = {}
+
+    if genres:
+        # Costruisci una regex per cercare qualsiasi genere specificato
+        regex_pattern = "|".join([re.escape(genre) for genre in genres])
+        filter_criteria["genre"] = {"$regex": regex_pattern, "$options": "i"}
+    if rating:
+        filter_criteria["rating"] = {"$gte": float(rating)}
+    if min_duration:
+        filter_criteria["duration"] = {"$gte": int(min_duration)}
+    if max_duration:
+        if "duration" in filter_criteria:
+            filter_criteria["duration"]["$lte"] = int(max_duration)
+        else:
+            filter_criteria["duration"] = {"$lte": int(max_duration)}
+
+    movies = list(collection.find(filter_criteria))
+    for movie in movies:
+        movie["_id"] = str(movie["_id"])
+
+    return jsonify(movies)
 
 
 # homepage
 @app.route("/")
 def homepage():
-    movies = list(collection.aggregate([{"$sample": {"size": 48}}]))
+    movies = list(collection.aggregate([{"$sample": {"size": 45}}]))
     return render_template("index.html", movies=movies)
 
 
--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,14 +1,121 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var movieYears = document.querySelectorAll(".movie-year");
-  movieYears.forEach(function (yearElem) {
-    var year = yearElem.textContent;
-    var yearInt = parseInt(year, 10);
-    if (!isNaN(yearInt)) {
-      yearElem.textContent = yearInt;
-    }
-  });
-});
+  // Elementi del DOM
+  var searchForm = document.getElementById("searchForm");
+  var searchInput = document.getElementById("searchInput");
+  var movieContainer = document.querySelector(".movie-container");
+  var filters = document.getElementById("filters");
+  var filterForm = document.getElementById("filterForm");
+  var filterToggle = document.querySelector(".filter-toggle");
 
+  // Inizializzazione
+  initializeEvents();
+
+  function initializeEvents() {
+
+    searchForm.addEventListener("submit", searchMovies);
+    filterForm.addEventListener("submit", applyFilters);
+    filterToggle.addEventListener("click", toggleFilterPanel);
+  }
+
+  function searchMovies(event) {
+    event.preventDefault();
+    var searchText = searchInput.value.trim();
+    fetch(`/search?query=${searchText}`)
+      .then(handleResponse)
+      .then(renderMovies)
+      .catch(handleError);
+  }
+
+  function applyFilters(event) {
+    event.preventDefault();
+    var params = new URLSearchParams(new FormData(filterForm)).toString();
+    fetch(`/api/filter?${params}`)
+      .then(handleResponse)
+      .then(renderMovies)
+      .catch(handleError);
+  }
+
+  function toggleFilterPanel() {
+    filters.style.display = filters.style.display === "none" ? "flex" : "none";
+  }
+
+  function renderMovies(movies) {
+    var movieHTML = "";
+    if (movies.length === 0) {
+      movieHTML = "<p>Film non trovato</p>";
+    } else {
+      movies.forEach(function (movie) {
+        movieHTML +=
+          '<div class="movie-card" data-movie-id="' +
+          movie._id +
+          '" onclick="location.href=\'/movie/' +
+          movie._id +
+          "'\">";
+        movieHTML +=
+          '<img src="' +
+          movie.poster +
+          '" alt="Poster of ' +
+          movie.title +
+          '">';
+        movieHTML += '<div class="movie-info">';
+        movieHTML +=
+          "<h2>" +
+          movie.title +
+          ' (<span class="movie-year">' +
+          movie.year +
+          "</span>)</h2>";
+        movieHTML += "<p><strong>Genre:</strong> " + movie.genre + "</p>";
+        movieHTML +=
+          "<p><strong>Certificate:</strong> " + movie.certificate + "</p>";
+        movieHTML +=
+          "<p><strong>Duration:</strong> " + movie.duration + " min</p>";
+        movieHTML +=
+          '<p><strong>Rating:</strong> <span class="rating" data-rating="' +
+          movie.rating +
+          '">' +
+          movie.rating +
+          "</span></p>";
+        movieHTML += "</div></div>";
+      });
+    }
+    movieContainer.innerHTML = movieHTML;
+  }
+
+  function parseYear(year) {
+    var yearInt = parseInt(year, 10);
+    return isNaN(yearInt) ? year : yearInt;
+  }
+
+  function applyRatingColors() {
+    var ratings = document.querySelectorAll(".rating");
+    ratings.forEach(function (rating) {
+      var value = parseFloat(rating.getAttribute("data-rating"));
+      if (value < 6) {
+        rating.style.color = "red";
+      } else if (value >= 6 && value < 7.5) {
+        rating.style.color = "yellow";
+      } else if (value >= 7.5 && value < 9) {
+        rating.style.color = "green";
+      } else if (value >= 9) {
+        rating.style.color = "purple";
+      }
+    });
+  }
+
+  function handleResponse(response) {
+    if (!response.ok) {
+      throw new Error("Errore nella richiesta al server: " + response.status);
+    }
+    return response.json();
+  }
+
+  function handleError(error) {
+    console.error("Si è verificato un errore:", error);
+    // Puoi gestire l'errore qui, ad esempio mostrando un messaggio all'utente
+  }
+
+ 
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   var movieCards = document.querySelectorAll(".movie-card");
@@ -17,79 +124,5 @@ document.addEventListener("DOMContentLoaded", function () {
       var movieId = card.getAttribute("data-movie-id");
       window.location.href = "/movie/" + movieId;
     });
-  });
-
-  var movieYears = document.querySelectorAll(".movie-year");
-  movieYears.forEach(function (yearElem) {
-    var year = yearElem.textContent;
-    var yearInt = parseInt(year, 10);
-    if (!isNaN(yearInt)) {
-      yearElem.textContent = yearInt;
-    }
-  });
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  var searchForm = document.getElementById("searchForm");
-  var searchInput = document.getElementById("searchInput");
-  var movieContainer = document.querySelector(".movie-container");
-
-  searchForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita il comportamento predefinito di invio del modulo
-    var searchText = searchInput.value.trim();
-
-    fetch("/search?query=" + searchText)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            "Errore nella richiesta al server: " + response.status
-          );
-        }
-        return response.json();
-      })
-      .then((data) => {
-        renderMovies(data);
-      })
-      .catch((error) => {
-        console.error("Si è verificato un errore:", error);
-        // Puoi gestire l'errore qui, ad esempio mostrando un messaggio all'utente
-      });
-  });
-
-  function renderMovies(movies) {
-    var movieHTML = "";
-    movies.forEach(function (movie) {
-      movieHTML += '<div class="movie-card" data-movie-id="' + movie._id + '">';
-      movieHTML +=
-        '<img src="' + movie.poster + '" alt="Poster of ' + movie.title + '">';
-      movieHTML += '<div class="movie-info">';
-      movieHTML +=
-        "<h2>" +
-        movie.title +
-        ' (<span class="movie-year">' +
-        movie.year +
-        "</span>)</h2>";
-      movieHTML += "<p><strong>Genre:</strong> " + movie.genre + "</p>";
-      movieHTML +=
-        "<p><strong>Certificate:</strong> " + movie.certificate + "</p>";
-      movieHTML +=
-        "<p><strong>Duration:</strong> " + movie.duration + " min</p>";
-      movieHTML +=
-        '<p><strong>Rating:</strong> <span class="rating">' +
-        movie.rating +
-        "</span></p>";
-      movieHTML += "</div></div>";
-    });
-    movieContainer.innerHTML = movieHTML;
-  }
-
-  var movieYears = document.querySelectorAll(".movie-year");
-  movieYears.forEach(function (yearElem) {
-    var year = yearElem.textContent;
-    var yearInt = parseInt(year, 10);
-    if (!isNaN(yearInt)) {
-      yearElem.textContent = yearInt;
-    }
   });
 });
