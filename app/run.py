1d0b29f2381682c147d273452782be0b67ec9936
dom <d.dantuono7@studenti.unisa.it>
2024-06-10 08:12:20 +0200

    aggiunta di login, registrazione e area preferiti.

--- app/run.py
+++ app/run.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, render_template, request
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import re, sys
+from datetime import datetime
+
 
 
 app = Flask(__name__)
@@ -13,6 +15,7 @@ sys.stdout.reconfigure(encoding="utf-8")
 client = MongoClient("mongodb://localhost:27017/")
 db = client["mydatabase"]
 collection = db["movies"]
+collection1 = db["users"]
 
 
 # Endpoint per mostrare le collezioni nel database
@@ -87,8 +90,143 @@ def filter_movies():
 @app.route("/")
 def homepage():
     movies = list(collection.aggregate([{"$sample": {"size": 45}}]))
+    '''
+    # parte per inserire i dati di un utente
+    
+     # Esempio di dati da inserire
+    film_visti = [
+    {"nome": "Inception", "data_di_visione": datetime(2021, 5, 21)},
+    {"nome": "The Matrix", "data_di_visione": datetime(2021, 6, 10)},
+    {"nome": "Interstellar", "data_di_visione": datetime(2021, 7, 15)}
+    ]
+
+    film_da_vedere = [
+    {"nome": "Inception"},
+    {"nome": "The Matrix"},
+    {"nome": "Interstellar"}
+    ]
+    collezione = db["users"]
+    collezione.insert_one({
+            "nome": 'pippo',
+            "cognome": 'franco',
+            "data_di_nascita": datetime(2021, 7, 15),
+            "username": 'franco',
+            "password": 'franco',
+            "film_visti": film_visti,
+            "film_da_vedere": film_da_vedere,
+        })
+    #fine inserimento 
+    
+    # parte per inserire i dati di un utente
+    
+     # Esempio di dati da inserire
+     
+    film_visti = [
+    {"nome": "My Spy", "data_di_visione": datetime(2021, 5, 21)},
+    {"nome": "The Incredibles", "data_di_visione": datetime(2021, 6, 10)},
+    ]
+
+    film_da_vedere = [
+    {"nome": "Foxcatcher"},
+    {"nome": "Sacrifice"},
+    ]
+    collezione = db["users"]
+    collezione.insert_one({
+            "nome": 'pippo',
+            "cognome": 'franco',
+            "data_di_nascita": datetime(2021, 7, 15),
+            "username": 'fra',
+            "password": 'franco',
+            "film_visti": film_visti,
+            "film_da_vedere": film_da_vedere,
+        })
+    #fine inserimento 
+    
+    for it in collezione.find():
+        print(it)
+    '''
     return render_template("index.html", movies=movies)
 
+# login
+@app.route("/login")
+def login():
+    movies = list(collection.aggregate([{"$sample": {"size": 45}}]))
+    return render_template("login.html", movies=movies)
+
+# Endpoint per verificare l'utente
+@app.route("/login", methods=["POST"])
+def login_user():
+     un = request.form['uname']
+     pas = request.form['psw']
+     documento= collection1.find_one({"username": un,"password":pas})
+     if documento: 
+         print('bravo_cesi')
+         preferiti=list()
+         vis=list()
+         for chiave,valore in documento.items():
+          if chiave == 'film_da_vedere':
+               for it in valore:
+                if it != 'NaN':
+                  print(it)
+                  tit=it.get('nome')
+                  print(tit)
+                  iter=collection.find_one({"title": tit})
+                  preferiti.append(iter)
+                  print(iter)
+          if chiave == 'film_visti':
+               for it in valore:
+                if it != 'NaN':
+                  print(it)
+                  tit=it.get('nome')
+                  print(tit)
+                  iter=collection.find_one({"title": tit})
+                  vis.append(iter)
+                  print(iter)
+         return render_template("preferiti.html", pref=preferiti, visti=vis)
+     else:
+          print('sgamato')
+          return render_template("login.html", errors="err")
+
+# reg
+@app.route("/reg", methods=["GET"])
+def reg():
+    movies = list(collection.aggregate([{"$sample": {"size": 45}}]))
+    return render_template("reg.html", movies=movies)
+
+# Endpoint per registrare l'utente
+@app.route("/reg", methods=["POST"])
+def reg_user():
+     nm = request.form['name']
+     sr = request.form['surname']
+     br = request.form['birthday']
+     un = request.form['uname']
+     pas = request.form['psw']
+     documento= collection1.find_one({"username": un})
+     if documento: 
+        print('sgamato')
+        return render_template("reg.html", errors="usr")
+     else:
+            collection1.insert_one({
+            "nome": nm,
+            "cognome": sr,
+            "data_di_nascita": br,
+            "username": un,
+            "password": pas,
+            "film_visti": "NaN",
+            "film_da_vedere": "NaN",
+         })
+            print('cesi')
+     usrs=collection1.find()       
+     for el in usrs:
+         print(el)
+     movies = list(collection.aggregate([{"$sample": {"size": 45}}]))
+     return render_template("reg.html", movies=movies)
+     
+# homepage
+@app.route("/preferiti")
+def pref():
+    movies = list(collection.find([{"$sample": {"size": 45}}]))
+    return render_template("preferiti.html", movies=movies)
 
 if __name__ == "__main__":
     app.run(debug=True)
